d62ef6c009d3cfece01d6d50161ec3c541af3269
prolajumokeoni <prolajumokeoni@gmail.com>
2021-11-13 16:07:17 +0100

    Update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { greetingReducer } from './greetings/Greetings';
+import greetingReducer from './greetings/Greetings';
 
 const reducer = combineReducers({
-  greetingReducer,
+  greeting: greetingReducer,
 });
-
 const store = createStore(
   reducer,
   applyMiddleware(thunk),
 );
-
 export default store;
