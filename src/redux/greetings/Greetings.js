1ceeab06dcfbf57fe6eb76a1052c3a0c6a2e64ce
prolajumokeoni <prolajumokeoni@gmail.com>
2021-11-13 16:06:32 +0100

    Update greeting reducer

--- src/redux/greetings/Greetings.js
+++ src/redux/greetings/Greetings.js
@@ -1,31 +1,49 @@
-const GET_GREETING = 'hello-rails-react/greeting/GET_GREETING';
-
-const initialValue = {};
-
-const getGreeting = (payload) => ({
-  type: GET_GREETING,
+const GET_GREETING_STARTED = 'rails-react-app/greeting/GET_GREETING_STARTED';
+const GET_GREETING_SUCCEED = 'rails-react-app/greeting/GET_GREETING_SUCCEED';
+const GET_GREETING_FAILED = 'rails-react-app/greeting/GET_GREETING_FAILED';
+const initialState = {
+  status: 'awaiting',
+  message: '',
+};
+const getGreetingStarted = () => ({
+  type: GET_GREETING_STARTED,
+});
+export const getGreetingSucceed = (payload) => ({
+  type: GET_GREETING_SUCCEED,
   payload,
 });
-
-const getGreetingThunk = () => async (dispatch) => {
-  const request = await fetch('http://localhost:3000/v1/greetings');
-  const response = await request.json();
-  const data = await response.greeting;
-  if (data) {
-    dispatch(getGreeting(data.message));
-  }
+const getGreetingFailed = (payload) => ({
+  type: GET_GREETING_FAILED,
+  payload,
+});
+export const fetchGreeting = () => async (dispatch) => {
+  const url = 'http://localhost:3001/v1/greetings';
+  dispatch(getGreetingStarted());
+  await fetch(url)
+    .then((response) => response.json())
+    .then((json) => dispatch(getGreetingSucceed(json)))
+    .catch((error) => dispatch(getGreetingFailed(error.toString())));
 };
-
-const greetingReducer = (state = initialValue, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case GET_GREETING:
+    case GET_GREETING_STARTED:
       return {
         ...state,
-        greetings: action.payload,
+        status: 'loading',
+      };
+    case GET_GREETING_SUCCEED:
+      return {
+        message: action.payload.message,
+        status: 'awaiting',
+      };
+    case GET_GREETING_FAILED:
+      return {
+        ...state,
+        status: 'failed',
+        error: action.payload,
       };
     default:
       return state;
   }
 };
-
-export { greetingReducer, getGreetingThunk };
+export default reducer;
